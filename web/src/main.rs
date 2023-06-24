7eaf98fa4f582504e3d831336ebefde7de4e0eae
fdc_mbp <fudongcheng110@foxmail.com>
2023-06-24 19:24:59 +0800

    【通过】网页 UI、路由 Demo实现

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,22 +1,20 @@
-use actix_web::{get, App, HttpResponse, HttpServer};
-use std::fs;
-use std::path::{PathBuf};
+use actix_files::{Files, NamedFile};
+use actix_web::{get, web, App, HttpResponse, HttpServer};
 use rand::seq::SliceRandom;
+use std::fs;
 use std::io::Read;
+use std::path::PathBuf;
 
-
-fn get_jpg_files(folder_path: &str) -> Vec<PathBuf> {
+fn get_image_paths(folder_path: &str) -> Vec<PathBuf> {
     let mut image_paths = Vec::new();
 
     if let Ok(entries) = fs::read_dir(folder_path) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                let path = entry.path();
-                if path.is_file() && path.extension().map(|ext| ext == "jpg").unwrap_or(false) {
-                    image_paths.push(path);
-                } else if path.is_dir() {
-                    image_paths.extend(get_jpg_files(&path.to_string_lossy()));
-                }
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if path.is_file() && path.extension().map(|ext| ext == "jpg").unwrap_or(false) {
+                image_paths.push(path);
+            } else if path.is_dir() {
+                image_paths.extend(get_image_paths(&path.to_string_lossy()));
             }
         }
     }
@@ -24,22 +22,23 @@ fn get_jpg_files(folder_path: &str) -> Vec<PathBuf> {
     image_paths
 }
 
-#[get("/")]
-async fn random_image() -> HttpResponse {
+#[get("/random/hero/image")]
+async fn random_hero_image() -> HttpResponse {
     // 从某个文件夹中获取所有图片文件路径
     let image_folder = "../heroes";
-    let image_paths = get_jpg_files(image_folder);
+    let image_paths = get_image_paths(image_folder);
 
     // 随机选择一个图片路径
     let random_image_path = image_paths
         .choose(&mut rand::thread_rng())
-        .unwrap_or(&PathBuf::from("default_image.jpg")).to_owned();
+        .unwrap_or(&PathBuf::from("default_image.jpg"))
+        .to_owned();
 
     // 读取图片数据
-    let mut img = fs::File::open(random_image_path).expect("Failed to open image file");
+    let mut img = fs::File::open(random_image_path).expect("图片打开失败");
     let mut buffer = Vec::new();
     if img.read_to_end(&mut buffer).is_err() {
-        return HttpResponse::InternalServerError().body("Failed to read image file");
+        return HttpResponse::InternalServerError().body("图片读取失败");
     }
 
     println!("{:?}", img);
@@ -47,14 +46,33 @@ async fn random_image() -> HttpResponse {
     HttpResponse::Ok().content_type("image/jpeg").body(buffer)
 }
 
+#[get("/random/hero")]
+async fn random_hero() -> actix_web::Result<NamedFile> {
+    Ok(NamedFile::open("./asset/random_hero.html")?)
+}
+
+#[get("/")]
+async fn index() -> actix_web::Result<NamedFile> {
+    Ok(NamedFile::open("./asset/index.html")?)
+}
+
+#[get("/favicon.ico")]
+async fn favicon() -> actix_web::Result<NamedFile> {
+    Ok(NamedFile::open("./asset/favicon.ico")?)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(random_image)
+            .service(favicon)
+            .service(index)
+            .service(random_hero)
+            .service(random_hero_image)
+            .default_service(web::route().to(HttpResponse::NotFound))
+            .service(Files::new("/", "./asset"))
     })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await
 }
